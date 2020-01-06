d64661097a7d74dc624efaf55266a4a297659175
Philipp Gerbig <p.gerbig@sensory-minds.com>
2020-01-06 14:30:40 +0100

    add file upload, change authentication error

--- src/modules/user/Me.resolver.ts
+++ src/modules/user/Me.resolver.ts
@@ -2,13 +2,14 @@ import { Resolver, Query, Ctx } from "type-graphql";
 
 import { User } from "../../entity/User";
 import { MyContext } from "../../types/MyContext";
+import { AuthenticationError } from "apollo-server-express";
 
 @Resolver()
 export class MeResolver {
   @Query(() => User, { nullable: true })
   async me(@Ctx() ctx: MyContext): Promise<User | undefined> {
     if (!ctx.req.session!.userId) {
-      return undefined;
+      throw new AuthenticationError('User is not authenticated');
     }
 
     return User.findOne(ctx.req.session!.userId);
